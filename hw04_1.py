a07f4407c36dcacf5a8c92f040d631550e59b748
haema5 <killy.ny@gmail.com>
2018-11-07 22:58:33 +0300

    Add hw03_5

--- hw04_1.py
+++ hw04_1.py
@@ -1,7 +1,8 @@
 # Проанализировать скорость и сложность одного - трёх любых алгоритмов,
 # разработанных в рамках домашнего задания первых трех уроков.
 #
-# 1 ( hw03_2 )
+# 1
+# ( hw03_2 )
 # Во втором массиве сохранить индексы четных элементов первого массива.
 # Например, если дан массив со значениями 8, 3, 15, 6, 4, 2, то во второй
 # массив надо заполнить значениями 1, 4, 5, 6 (или 0, 3, 4, 5 - если
@@ -38,7 +39,8 @@ def s_even(array_len):
 # 100 loops, best of 5: 19.6 msec per loop
 
 
-# 2 ( hw03_3 - Первый способ)
+# 2
+# ( hw03_3 - Первый способ)
 # В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
 
 def min_max(array_len):
@@ -72,7 +74,8 @@ def min_max(array_len):
 # 100 loops, best of 5: 19.6 msec per loop
 
 
-# 2 ( hw03_3 - Второй способ)
+# 2
+# ( hw03_3 - Второй способ)
 # В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
 
 def min_max_inc(array_len):
@@ -84,6 +87,7 @@ def min_max_inc(array_len):
     array[min_pos], array[max_pos] = array[max_pos], array[min_pos]
 
     return array
+
 
 # cProfile.run('min_max_inc(100)')
 # 1    0.000    0.000    0.000    0.000 hw04_1.py:75(min_max_inc)
@@ -99,4 +103,68 @@ def min_max_inc(array_len):
 # "hw04_1.min_max_inc(10000)"
 # 100 loops, best of 5: 18 msec per loop
 
+# 3
+# ( hw03_5 - Первый способ)
+# В массиве найти максимальный отрицательный элемент. Вывести на экран его
+# значение и позицию в массиве.
+
+
+def hw03_5(arr_len):
+    arr = [randint(-100, 100) for _ in range(arr_len)]
+    min_el = arr[0]
+    for i in arr:
+        if i < min_el:
+            min_el = i
+
+    if min_el < 0:
+        max_el = min_el
+        for i in arr:
+            if max_el < i < 0:
+                max_el = i
+        return max_el, arr.index(max_el)
+    else:
+        return 'Ошибка. В массиве нет отрицательных элементов.'
+
+
+# cProfile.run('hw03_5(100000)')
+# 1    0.007    0.007    0.171    0.171 hw04_1.py:8(hw03_5)
+# cProfile.run('hw03_5(1000000)')
+# 1    0.067    0.067    1.632    1.632 hw04_1.py:8(hw03_5)
+# cProfile.run('hw03_5(10000000)')
+# 1    0.670    0.670   16.003   16.003 hw04_1.py:8(hw03_5)
 #
+# "hw04_1.hw03_5(100000)"
+# 10 loops, best of 3: 103 msec per loop
+# "hw04_1.hw03_5(1000000)"
+# 10 loops, best of 3: 1.06 sec per loop
+# "hw04_1.hw03_5(10000000)"
+# 10 loops, best of 3: 10.6 sec per loop
+
+
+# ( hw03_5 - Второй способ)
+
+def hw03_5_modify(arr_len):
+    arr = [randint(-100, 100) for _ in range(arr_len)]
+    new_arr = [i for i in arr if i < 0]
+    if len(new_arr) > 0:
+        max_el = max(new_arr)
+        return max_el, arr.index(max_el)
+    else:
+        return 'Ошибка. В массиве нет отрицательных элементов.'
+
+# cProfile.run('hw03_5_modify(100000)')
+# 1    0.000    0.000    0.154    0.154 hw04_1.py:25(hw03_5_modify)
+# cProfile.run('hw03_5_modify(1000000)')
+# 1    0.000    0.000    1.589    1.589 hw04_1.py:25(hw03_5_modify)
+# cProfile.run('hw03_5_modify(10000000)')
+# 1    0.000    0.000   16.096   16.096 hw04_1.py:25(hw03_5_modify)
+#
+# "hw04_1.hw03_5_modify(100000)"
+# 10 loops, best of 3: 101 msec per loop
+# "hw04_1.hw03_5_modify(1000000)"
+# 10 loops, best of 3: 1.05 sec per loop
+# "hw04_1.hw03_5_modify(10000000)"
+# 10 loops, best of 3: 10.6 sec per loop
+
+# Вывод:
+# На тестируемых объемах массивов разница не принципиальна.
