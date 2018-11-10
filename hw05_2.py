2508646c782a30949d8442539c0e01d17380952b
haema5 <killy.ny@gmail.com>
2018-11-10 15:22:32 +0300

    Изменил подход к решению

--- hw05_2.py
+++ hw05_2.py
@@ -5,47 +5,54 @@
 # Сохранить их как [‘A’, ‘2’] и [‘C’, ‘4’, ‘F’] соответственно.
 # Сумма чисел из примера: [‘C’, ‘F’, ‘1’].
 # Произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
-from collections import deque, Counter
+from collections import deque
 
-# hd = {'0': '0', '1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9',
-#       'A': '10', 'B': '11', 'C': '12', 'D': '13', 'E': '14', 'F': '15'}
-hd = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
-      'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
-dh = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
-      'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
-answer = deque()
-print(hd)
 
-a = deque(str.upper('A2'))
-a.reverse()
-b = deque(str.upper('C4F'))
-b.reverse()
-print(a, b, sep=',\t')
-zn = '+'
-if a < b:
-    b, a = a, b
-    print(a, b, sep=',\t')
+def input_format(a, b):
+    num_1 = deque(a)
+    num_2 = deque(b)
+    return num_1, num_2
 
-ost = 0
-for d in range(len(a)):
-    if d > len(b)-1:
-        summa = hd[a[d]] + ost
-        ost = 0
-        if summa > 15:
-            ost = summa // 15
-            summa = summa % 15
-    else:
-        summa = hd[a[d]] + hd[b[d]] + ost
-        ost = 0
-        if summa > 15:
-            ost = summa // 15
-            summa = summa % 15
-    answer.appendleft(summa)
 
-print(answer)
-for i in answer:
-    print(i)
-    print(answer.index(i))
-    print(hd[str(i)])
-    answer[answer.index(i)] = hd[i]
-print(answer)
+def toDec(hexa):
+    hex_dec = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+               'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
+    dec = 0
+    deg = len(hexa) - 1
+    for var in hexa:
+        dec += hex_dec[var] * 16 ** deg
+        deg -= 1
+    return dec
+
+
+def toHex(dec):
+    dec_hex = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9',
+               10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+    hexa = ''
+    if dec == 0:
+        return '0'
+    while dec != 0:
+        hexa += dec_hex[dec % 16]
+        dec = dec // 16
+    return hexa[::-1]
+
+
+def oper_mul(num_1, num_2):
+    result = toHex(toDec(num_1) * toDec(num_2))
+    return result
+
+
+def oper_sum(num_1, num_2):
+    result = toHex(toDec(num_1) + toDec(num_2))
+    return result
+
+
+a = str.upper(input('Введите число А (HEX): '))
+b = str.upper(input('Введите число B (HEX): '))
+
+num_1, num_2 = input_format(a, b)
+summ = oper_sum(num_1, num_2)
+mult = oper_mul(num_1, num_2)
+
+print(f'\nCумма чисел {a} и {b} = {summ};')
+print(f'Произведение чисел {a} и {b} = {mult}.')
